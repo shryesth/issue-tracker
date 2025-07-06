66c1885d569372f9748360bf71e8e80297928868
Shryesth Pandey <shryesth4236@gmail.com>
2025-07-07 03:26:14 +0530

    build api for creating issues

---
+++ prisma/migrations/20250706201307_create_issue/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Issue" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'OPEN',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
